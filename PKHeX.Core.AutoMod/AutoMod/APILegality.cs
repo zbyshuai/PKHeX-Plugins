105a97cd5c6c12e12ad4d055ecee9e380292c12a
Santacrab2 <skaradiodj@gmail.com>
2023-08-22 07:28:48 -0400

    gotta convert otherwise sysbot will inject bad eggs woopsie

--- PKHeX.Core.AutoMod/AutoMod/APILegality.cs
+++ PKHeX.Core.AutoMod/AutoMod/APILegality.cs
@@ -100,55 +100,57 @@ namespace PKHeX.Core.AutoMod
                 // Create the PKM from the template.
                 var tr = SimpleEdits.IsUntradeableEncounter(enc) ? dest : GetTrainer(regen, enc.Version, enc.Generation);
                 var raw = enc.ConvertToPKM(tr, criteria);
+                var pk = EntityConverter.ConvertToType(raw, destType, out _);
                 if (enc is EncounterTrade8b && enc.Species == (ushort)Species.Magikarp)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    
+                    return pk;
                 }
                 if(enc is EncounterStatic3XD && enc.Species == (ushort)Species.Eevee)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if(enc.Context == EntityContext.Gen2 && enc.Species != (ushort)Species.Poliwag)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if(enc is EncounterStatic4 && enc.Species == (ushort)Species.Giratina)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    if (raw.Form == 1)
-                        raw.HeldItem = 112;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    if (pk.Form == 1)
+                        pk.HeldItem = 112;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if(enc is EncounterStatic4Pokewalker)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if (enc is EncounterStatic5Radar)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if (enc is EncounterTrade5BW)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if(enc is EncounterTrade5B2W2 && enc.Species == (ushort)Species.Gigalith)
                 {
                     satisfied = LegalizationResult.Regenerated;
-                    raw.CurrentHandler = 1;
-                    return raw;
+                    pk.CurrentHandler = 1;
+                    return pk;
                 }
                 if (raw.OT_Name.Length == 0)
                 {
@@ -167,7 +169,7 @@ namespace PKHeX.Core.AutoMod
                     raw = basepk1.ConvertToPK2();
 
                 // Bring to the target generation and filter
-                var pk = EntityConverter.ConvertToType(raw, destType, out _);
+                 pk = EntityConverter.ConvertToType(raw, destType, out _);
                 if (pk == null)
                     continue;
                 if (EntityConverter.IsIncompatibleGB(pk, template.Japanese, pk.Japanese))
