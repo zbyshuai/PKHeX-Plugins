a1a551e367690fa4a878fbd3915a1a87e0739fbc
Santacrab2 <skaradiodj@gmail.com>
2023-12-22 08:29:14 -0500

    - version bump

--- PKHeX.Core.Injection/BotController/Controllers/SysBotMini.cs
+++ PKHeX.Core.Injection/BotController/Controllers/SysBotMini.cs
@@ -174,14 +174,13 @@ namespace PKHeX.Core.Injection
             int byteCount = data.Length;
             for (int i = 0; i < byteCount; i += maxlength)
             {
-
                 var ba = SliceSafe(data,i,maxlength);
                 WriteBytes(ba, offset, method);
                 offset += maxlength;
                 chunk++;
             }
         }
-        public byte[] SliceSafe(ReadOnlySpan<byte> src, int offset, int length)
+        public static byte[] SliceSafe(ReadOnlySpan<byte> src, int offset, int length)
         {
             var delta = src.Length - offset;
             if (delta < length)
